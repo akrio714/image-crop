949ec1896e53506947583fb72b1924d29ccf0082
赵昭 <zhaozhao@zhaozhaodeMacBook-Pro-2.local>
2019-09-05 06:51:54 +0800

    更新

---
+++ src/components/CropModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-dialog title="收货地址"
+               :visible.sync="dialogTableVisible">
+      <div>1111</div>
+    </el-dialog>
+</template>
+<script>
+export default {
+  name: 'CropModalComponent',
+  data(){
+      return {
+          dialogTableVisible:true
+      }
+  }
+}
+</script>
---
+++ src/views/Demo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="demo-page">
+    111
+      <crop-modal></crop-modal>
+  </div>
+</template>
+
+<script>
+import CropModal from '../components/CropModal'
+export default {
+  name: 'DemoPage',
+  components:{CropModal}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.demo-page{
+    width: 100vw;
+    height: 100vh;
+}
+</style>
