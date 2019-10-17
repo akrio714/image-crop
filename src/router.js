f31a0f34542bb4f5c4b970e1f518de3b542e29e2
赵昭 <akrionsmail@gmail.com>
2019-10-17 17:24:36 +0800

    更新部分

--- src/router.js
+++ src/router.js
@@ -2,14 +2,20 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Demo from './views/Demo.vue'
 import Home from './views/Home.vue'
+import Crop from './views/Crop.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/crop',
+      path: '/demo',
       name: 'Demo',
       component: Demo
+    },
+    {
+      path: '/crop',
+      name: 'Crop',
+      component: Crop
     },
     {
       path: '/',
